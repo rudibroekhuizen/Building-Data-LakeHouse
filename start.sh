c12ec36ab2ac854a76d4c4d3fdd1eb259531f6e6
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2023-01-06 23:45:40 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,11 @@
 set -euo pipefail
 
 echo -e "Build containers..."
-docker build -t presto:0.272.1 ./Dockerfiles/presto
 docker build -t cluster-apache-spark:3.1.1 Dockerfiles/spark
+docker build -t presto:0.272.1 ./Dockerfiles/presto
 
 echo -e "Start project..."
 docker compose up -d
 
 echo -e "Setup Minio"
 docker compose exec minio bash '/opt/scripts/setup.sh'
-
