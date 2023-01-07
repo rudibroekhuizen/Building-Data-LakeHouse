8e6b8da87f839739b727e0cefd7cbe4b2a6395bf
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2023-01-07 11:52:21 +0100

    Update 20-setup.sql

--- postgres/docker-entrypoint-initdb.d/20-setup.sql
+++ postgres/docker-entrypoint-initdb.d/20-setup.sql
@@ -14,6 +14,16 @@ CREATE TABLE bird (
    beak_size double precision NOT NULL
 );
 
+CREATE TABLE bat (
+   id bigint PRIMARY KEY DEFAULT nextval('seq'),
+   wingspan numeric NOT NULL,
+   body_temperature numeric NOT NULL
+);
+
 INSERT INTO bird (wingspan, beak_size)
 SELECT 20 + random() * 5, 2 + random()
+FROM generate_series(1, 1000000);
+
+INSERT INTO bat (wingspan, body_temperature)
+SELECT 15 + random() * 5, 40 + random() * 2
 FROM generate_series(1, 1000000);
